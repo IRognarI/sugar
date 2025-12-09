d45c60954f76fe248f8e9b9f1dd71cb67061c751
rognar <i@an-98.ru>
2025-12-10 01:58:51 +0300

    Тесты работают. Бота нужно писать заново. Запускать через конфигурации в IDEA

--- src/main/java/sugar/telegram/BotConfiguration.java
+++ src/main/java/sugar/telegram/BotConfiguration.java
@@ -1,17 +1,19 @@
 package sugar.telegram;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BotConfiguration {
 
-    @Bean
-    public UpdateConsumer  updateConsumer() {
-        String token = System.getenv("TELEGRAM_BOT_TOKEN");
+    @Value("${telegram.bot.token}")
+    private String token;
 
+    @Bean
+    public UpdateConsumer updateConsumer() {
         if (token == null || token.isEmpty()) {
-            throw new IllegalArgumentException(token + " не задан!");
+            throw new IllegalArgumentException("TELEGRAM_BOT_TOKEN не задан! Укажите в application.properties или через переменную окружения TELEGRAM_BOT_TOKEN");
         }
 
         return new UpdateConsumer(token);
